78f66e9dddd03f88142ed46765c989fd4823a90d
Rhys Rushton <rhys.rushton@gmail.com>
2020-04-17 13:41:01 +1000

    prompt done

---
+++ Develop/index.js
@@ -0,0 +1,75 @@
+var inquirer = require("inquirer");
+
+const questions = [
+    inquirer.prompt([
+        {
+        type: "input",
+        message: "What is your Github user name?",
+        name: "username"
+        },
+        {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "Title",       
+        },
+        {
+        type: "input",
+        message: "Provide a Description for your project",
+        name: "Description"       
+        },
+        {
+        type: "input",
+        message: "What is the installation protocol for this project?",
+        name: "Installation"       
+        },
+        {
+        type: "input",
+        message: "What usage infromation would you like to provide?",
+        name: "usage"       
+        },
+        {
+        type: "input",
+        message: "What is the liesence for your application?",
+        name: "lisence info"       
+        },
+        {
+        type: "input",
+        message: "What are the contribution reuirments for this project?",
+        name: "contribute"       
+        },
+        {
+        type: "input",
+        message: "What tests have you run with this application?",
+        name: "tests"       
+        },
+        {
+        type: "input",
+        message: "What would you like to put in the questions section?",
+        name: "questions"       
+        },
+        {   
+        type: "input",
+        message: "What is your email?",
+        name: "email"       
+        },
+    ])
+
+
+];
+
+
+
+
+
+
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
+
+
